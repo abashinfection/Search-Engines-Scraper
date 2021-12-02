5185c68cd8c2879a79b2903b503901982b7d1065
ABI <a.bash.infection@gmail.com>
2021-12-02 08:06:47 +0100

    Accept Search Tools Date parameter in Bing (not used tho)

--- search_engines/engines/bing.py
+++ search_engines/engines/bing.py
@@ -4,33 +4,37 @@ from ..config import PROXY, TIMEOUT, FAKE_USER_AGENT
 
 class Bing(SearchEngine):
     '''Searches bing.com'''
+
     def __init__(self, proxy=PROXY, timeout=TIMEOUT):
         super(Bing, self).__init__(proxy, timeout)
         self._base_url = 'https://www.bing.com'
-        self.set_headers({'User-Agent':FAKE_USER_AGENT})
+        self.set_headers({'User-Agent': FAKE_USER_AGENT})
+        self._set_search_tools({
+            'date': {
+            }
+        })
 
     def _selectors(self, element):
         '''Returns the appropriate CSS selector.'''
         selectors = {
-            'url': 'a[href]', 
-            'title': 'h2', 
-            'text': 'p', 
-            'links': 'ol#b_results > li.b_algo', 
+            'url': 'a[href]',
+            'title': 'h2',
+            'text': 'p',
+            'links': 'ol#b_results > li.b_algo',
             'next': 'div#b_content nav[role="navigation"] a.sb_pagN'
         }
         return selectors[element]
-    
-    def _first_page(self):
+
+    def _first_page(self, timeframe=None):  # NOTE: unused timeframe
         '''Returns the initial page and query.'''
         url = u'{}/search?q={}'.format(self._base_url, self._query)
-        return {'url':url, 'data':None}
-    
+        return {'url': url, 'data': None}
+
     def _next_page(self, tags):
         '''Returns the next page URL and post data (if any)'''
         selector = self._selectors('next')
         next_page = self._get_tag_item(tags.select_one(selector), 'href')
         url = None
         if next_page:
-            url = (self._base_url + next_page) 
-        return {'url':url, 'data':None}
-
+            url = (self._base_url + next_page)
+        return {'url': url, 'data': None}
