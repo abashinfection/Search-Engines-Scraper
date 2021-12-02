4f898f239b89f7d5531f79ba183c39c4398794e1
ABI <a.bash.infection@gmail.com>
2021-12-02 08:01:03 +0100

    Accept Search Tools Date parameter in AOL (not used tho)

--- search_engines/engines/aol.py
+++ search_engines/engines/aol.py
@@ -4,14 +4,23 @@ from ..config import PROXY, TIMEOUT
 
 class Aol(Yahoo):
     '''Seaches aol.com'''
+
     def __init__(self, proxy=PROXY, timeout=TIMEOUT):
         super(Aol, self).__init__(proxy, timeout)
         self._base_url = u'https://search.aol.com'
+        self._set_search_tools({
+            'date': {
+                '1h': 'tbs=qdr:h',
+                '1d': 'tbs=qdr:d',
+                '1w': 'tbs=qdr:w',
+                '1m': 'tbs=qdr:m',
+                '1y': 'tbs=qdr:y'
+            }
+        })
 
-    def _first_page(self):
+    def _first_page(self, timeframe=None):  # NOTE: unused timeframe
         '''Returns the initial page and query.'''
         url_str = u'{}/aol/search?q={}&ei=UTF-8&nojs=1'
         url = url_str.format(self._base_url, self._query)
         self._http_client.get(self._base_url)
-        return {'url':url, 'data':None}
-
+        return {'url': url, 'data': None}
